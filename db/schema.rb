cd8c4debfbec578d0bb8e86c4325b9b710c73410
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-04-29 17:15:39 -0500

    Run paperclip migration for image in article_image

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429220858) do
+ActiveRecord::Schema.define(version: 20150429221459) do
 
   create_table "article_images", force: :cascade do |t|
     t.integer  "article_id"
     t.text     "caption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "articles", force: :cascade do |t|
