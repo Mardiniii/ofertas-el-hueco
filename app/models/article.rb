58c32d99eaef3ab6bc2e85cb1c4abb037f49fec9
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-04-29 19:02:39 -0500

    Add dependent destroy to the models

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,7 +16,7 @@
 
 class Article < ActiveRecord::Base
 	belongs_to :storehouse
-	has_many :article_images
+	has_many :article_images, dependent: :destroy
 	accepts_nested_attributes_for :article_images, :limit => 5, :reject_if => :all_blank, allow_destroy: true
   validates_associated :article_images
 end
--- app/models/storehouse.rb
+++ app/models/storehouse.rb
@@ -22,5 +22,5 @@ class Storehouse < ActiveRecord::Base
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	belongs_to :user
-	has_many :articles
+	has_many :articles, dependent: :destroy
 end
