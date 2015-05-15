6cd7ef79bbb089abab2ea8e8837b0b7c01480daa
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-05-14 21:09:17 -0500

    Implement the from_omniauth method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,5 +38,14 @@ class User < ActiveRecord::Base
 	after_initialize :set_default_role, :if => :new_record?
 	def set_default_role
     self.role ||= :user
-  end         
+  end  
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+    end
+  end       
 end
