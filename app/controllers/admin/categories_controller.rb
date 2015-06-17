d8507708a914b96fe0cead0c76c6219635bd02c8
alejoescobar <alejoescobac@gmail.com>
2015-06-17 18:00:57 -0500

    Add show to admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,6 +4,10 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def show
+    @category = Category.find(params[:id])
+  end
+
   def new
     @category = Category.new
   end
