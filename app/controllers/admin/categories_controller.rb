a182d3634c1b578aaf94fb354d1efe491f2ee3a6
alejoescobar <alejoescobac@gmail.com>
2015-06-17 11:01:44 -0500

    Finish new and create actions for categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,3 +1,31 @@
 class Admin::CategoriesController < ApplicationController
 
+  def index
+
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "La categoría #{@category.name} fue creada con éxito"
+      redirect_to root_path
+    else
+      flash[:alert] = "Ha ocurrido un error y la categoría no ha sido almacenada"
+      render :action => 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+
 end
