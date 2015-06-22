cebcb0f0bdcd42f9581511fc06a386680cce375c
German Hernandez <muygrafico@gmail.com>
2015-06-22 18:46:40 -0500

    limit to add only one favorite product to my wishlist

--- app/controllers/whishlist_items_controller.rb
+++ app/controllers/whishlist_items_controller.rb
@@ -4,10 +4,15 @@ class WhishlistItemsController < ApplicationController
     @whishlist_items = User.find( current_user ).wishlist_items
   end
   def create
-    @whishlist_item = WishlistItem.new()
-    @whishlist_item.article_id = params[:whishlist_item_id]
-    @whishlist_item.user_id = current_user.id
-    @whishlist_item.save
+
+    if WishlistItem.where(user_id: current_user.id, article_id: params[:whishlist_item_id]).empty?
+      @whishlist_item = WishlistItem.new()
+      @whishlist_item.article_id = params[:whishlist_item_id]
+      @whishlist_item.user_id = current_user.id
+      @whishlist_item.save
+    else
+      flash[:alert] = "Ya agregaste este item a tu lista"
+    end
     redirect_to :back
     rescue ActionController::RedirectBackError
     redirect_to root_path
