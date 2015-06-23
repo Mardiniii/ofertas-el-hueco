e8079e2284dd00bd0a7cbf21eb6d3b68ab9c2e3c
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-06-23 11:54:52 -0500

    Add browse products for general items in the platform and for an specifical store

--- app/models/wishlist_item.rb
+++ app/models/wishlist_item.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: wishlist_items
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  article_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class WishlistItem < ActiveRecord::Base
   belongs_to :article
   belongs_to :user
