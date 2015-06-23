3d3990bdc0ea0ccc0c0b4c289bbea4e315394fc9
German Hernandez <muygrafico@gmail.com>
2015-06-23 13:12:52 -0500

    add modal for storehouse info related to an article

--- app/models/storehouse.rb
+++ app/models/storehouse.rb
@@ -23,4 +23,23 @@ class Storehouse < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	belongs_to :user
 	has_many :articles, dependent: :destroy
+
+  def image
+    Storehouse.find( self.id ).avatar.url
+  end
+  def awesome_telephone
+    ActionController::Base.helpers.number_to_phone( self.telephone )
+    # ActionController::Base.helpers.number_with_delimiter(self.price, :delimiter => ',')
+  end
+  def awesome_whatsapp
+    ActionController::Base.helpers.number_to_phone( self.whatsapp )
+    # ActionController::Base.helpers.number_with_delimiter(self.price, :delimiter => ',')
+  end
+
+
+  def as_json(options = { })
+    super((options || { }).merge({
+      :methods => [:image, :awesome_telephone, :awesome_whatsapp]
+      }))
+  end
 end
